40fd5a39a8a05b576f317dcc65bbfcf4bf6ee16c
Tomasz Rojek <tomekr@gmail.com>
2019-07-18 19:59:06 +0200

    Modify ListContainer - DONE

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -4,12 +4,19 @@ import {getColumnsForList,createAction_addColumn} from './../../redux/columnsRed
 
 const mapDispatchToProps = (dispatch, props) => ({
   addColumn: title => dispatch(createAction_addColumn({
-    listId: props.id,
+    listId: props.match.params.id,
     title,
   })),
 });
-const mapStateToProps = (state, props) => ({
-  columns: getColumnsForList(state, props.id),
-});
+const mapStateToProps = (state, props) => {
+  const id = props.match.params.id;
+  const filteredLists = state.lists.filter(list => list.id == id);
+  const listParams = filteredLists[0] || {};
+
+  return {
+    ...listParams,
+    columns: getColumnsForList(state, id),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
