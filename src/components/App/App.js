67ce3c7a061c105ecd3f8d43f2950b009e5bf459
Tomasz Rojek <tomekr@gmail.com>
2019-07-18 15:33:58 +0200

    Add  routing to list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import MainLayout from './../MainLayout/MainLayout';
 import Faq from '../Faq/Faq';
 import {AnimatedSwitch} from 'react-router-transition';
 import styles from './App.scss';
+import List from '../List/ListContainer';
 
 const App = () => (
   <BrowserRouter>
@@ -19,6 +20,7 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
+        <Route exact path="/list/:id" component={List} />
       </AnimatedSwitch> 
     </MainLayout>
   </BrowserRouter>
